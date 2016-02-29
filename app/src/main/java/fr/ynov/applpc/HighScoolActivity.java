7b30c3acfdbd9181dd58d42e5ff1397c974c2e92
Pierre_Leroy <pierre.leroy@y-nov.com>
2016-02-29 09:42:21 +0100

    HighSchoolActivity XML

--- app/src/main/java/fr/ynov/applpc/HighScoolActivity.java
+++ app/src/main/java/fr/ynov/applpc/HighScoolActivity.java
@@ -10,4 +10,29 @@ public class HighScoolActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_scool);
     }
+
+    public void redirectCDI() {
+        //  Intent i = new Intent(this, YourOtherActivity.class);
+        //  startActivity(i);
+    }
+
+    public void redirectCVL() {
+        //   Intent i = new Intent(this, YourOtherActivity.class);
+        //   startActivity(i);
+    }
+
+    public void redirectCoordonnees() {
+        //   Intent i = new Intent(this, YourOtherActivity.class);
+        // startActivity(i);
+    }
+
+    public void redirectOrientation() {
+        //   Intent i = new Intent(this, YourOtherActivity.class);
+        //    startActivity(i);
+    }
+
+    public void redirectInfos() {
+        //   Intent i = new Intent(this, YourOtherActivity.class);
+        //    startActivity(i);
+    }
 }
