555dc52dd3e03e08f73011f3999c34d4727965ee
clément romac <clment romac>
2016-03-12 22:00:44 +0100

    custom adapter

--- app/src/main/java/fr/ynov/applpc/CdiActivity.java
+++ app/src/main/java/fr/ynov/applpc/CdiActivity.java
@@ -14,16 +14,13 @@ public class CdiActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cdi);
 
-        ArrayList<ModelInfo> arrayModelInfo = new ArrayList<>();
-        ModelInfoAdapter adapter = new ModelInfoAdapter(this, arrayModelInfo);
-        ListView listView = (ListView) findViewById(R.id.listview);
-        listView.setAdapter(adapter);
+        ArrayList<String[]> news = new ArrayList<String[]>();
+        for (int i = 0; i <= 10; i++) {
 
-        for (int i = 0; i <= 10; i++){
-
-            ModelInfo newInfo = new ModelInfo("Titre"+i, "text"+i);
-            adapter.add(newInfo);
-
+            news.add(new String[]{"Titre" + i, "text" + i});
         }
+        CustomAdapter myAdapter = new CustomAdapter(this, news, R.layout.list_item_view, new int[]{R.id.title_list_item, R.id.text_list_item});
+        ListView listView = (ListView) findViewById(R.id.listview_cdi);
+        listView.setAdapter(myAdapter);
     }
 }
---
+++ app/src/main/java/fr/ynov/applpc/CustomAdapter.java
@@ -0,0 +1,41 @@
+package fr.ynov.applpc;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Nathan on 03/03/2016.
+ */
+public class CustomAdapter extends ArrayAdapter<String[]> {
+    private int list_item_view;
+    private int[] list_item_views;
+
+    public CustomAdapter(Context context, ArrayList<String[]> param, int list_item_view, int[] list_item_views) {
+        super(context, 0, param);
+        this.list_item_view = list_item_view;
+        this.list_item_views = list_item_views;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        String[] content = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(list_item_view, parent, false);
+        }
+
+        // Fill the TextView
+        for(int i =0; i<list_item_views.length; i++){
+            ((TextView) convertView.findViewById(list_item_views[i])).setText(content[i]);
+        }
+        // Return the completed view to render on screen
+        return convertView;
+    }
+}
