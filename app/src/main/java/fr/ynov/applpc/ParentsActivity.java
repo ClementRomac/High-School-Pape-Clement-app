b65d0395325eaed4b435af2fc36ecad2232abf0e
Cédric Prezelin <cedric.prezelin@outlook.fr>
2016-03-12 12:30:39 +0100

    Rectification listview et add parent_news.xml

--- app/src/main/java/fr/ynov/applpc/ParentsActivity.java
+++ app/src/main/java/fr/ynov/applpc/ParentsActivity.java
@@ -19,6 +19,8 @@ public class ParentsActivity extends AppCompatActivity {
         }
     };
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
