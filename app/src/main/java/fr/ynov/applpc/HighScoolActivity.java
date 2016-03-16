4414ea3bfdf300b171261306c206b547ae92c0a0
Pierre_Leroy <pierre.leroy@y-nov.com>
2016-03-16 16:11:45 +0100

    boutons high school

--- app/src/main/java/fr/ynov/applpc/HighScoolActivity.java
+++ app/src/main/java/fr/ynov/applpc/HighScoolActivity.java
@@ -6,9 +6,57 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class HighScoolActivity extends ActionBarActivity {
 //Pierre
+
+    private RelativeLayout cdi = null;
+    private RelativeLayout cvl = null;
+    private RelativeLayout coordonnes = null;
+    private RelativeLayout orientation = null;
+    private RelativeLayout infos = null;
+
+    private View.OnClickListener redirect_cdi = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(HighScoolActivity.this, CdiActivity.class);
+
+            startActivity(intent);
+        }
+    };
+    private View.OnClickListener redirect_cvl = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(HighScoolActivity.this, CvlActivity.class);
+
+            startActivity(intent);
+        }
+    };
+    private View.OnClickListener redirect_coordonnes = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(HighScoolActivity.this, ContactActivity.class);
+
+            startActivity(intent);
+        }
+    };
+    private View.OnClickListener redirect_orientation = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(HighScoolActivity.this, OrietationActivity.class);
+
+            startActivity(intent);
+        }
+    };
+    private View.OnClickListener redirect_infos = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(HighScoolActivity.this, InfosActivity.class);
+
+            startActivity(intent);
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,18 +64,46 @@ public class HighScoolActivity extends ActionBarActivity {
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.accueil_bck_lycee)));
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.high_school_fleche_retour);
         setContentView(R.layout.activity_high_scool);
-        //Temporaire
-            final Button buttonCdi = (Button) findViewById(R.id.CDI);
-            buttonCdi.setOnClickListener(new View.OnClickListener() {
 
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(HighScoolActivity.this, CdiActivity.class);
-                    startActivity(intent);
-                }
-            });
-        //Fin temporaire
+        cdi = (RelativeLayout) findViewById(R.id.relative_CDI);
+        cvl = (RelativeLayout) findViewById(R.id.relative_CVL);
+        coordonnes = (RelativeLayout) findViewById(R.id.relative_coordonnes);
+        orientation = (RelativeLayout) findViewById(R.id.relative_orientation);
+        infos = (RelativeLayout) findViewById(R.id.relative_infos);
+
+        cdi.setOnClickListener(redirect_cdi);
+        cvl.setOnClickListener(redirect_cvl);
+        coordonnes.setOnClickListener(redirect_coordonnes);
+        orientation.setOnClickListener(redirect_orientation);
+        infos.setOnClickListener(redirect_infos);
+
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public void redirectCDI() {
         //  Intent i = new Intent(this, YourOtherActivity.class);
