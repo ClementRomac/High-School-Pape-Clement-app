c35e054cfc8b0077dc2f2284a14d2279588b1a85
clément romac <clment romac>
2016-03-20 16:05:35 +0100

    page lycée

--- app/src/main/java/fr/ynov/applpc/HighScoolActivity.java
+++ app/src/main/java/fr/ynov/applpc/HighScoolActivity.java
@@ -5,21 +5,27 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import fr.ynov.applpc.data.DataCDIProvider;
+import fr.ynov.applpc.data.DataCVLProvider;
+import fr.ynov.applpc.data.DataHighSchoolProvider;
 
 public class HighScoolActivity extends ActionBarActivity {
 //Pierre
 
-    private RelativeLayout cdi = null;
-    private RelativeLayout cvl = null;
-    private RelativeLayout coordonnes = null;
-    private RelativeLayout orientation = null;
-    private RelativeLayout infos = null;
+    private LinearLayout cdi = null;
+    private LinearLayout cvl = null;
+    private LinearLayout coordonnes = null;
+    private LinearLayout orientation = null;
+    private LinearLayout infos = null;
 
     private View.OnClickListener redirect_cdi = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            findViewById(R.id.fleche_cdi).setBackgroundResource(R.drawable.flechecdi);
+            findViewById(R.id.iv_highschool_fleche_cdi).setBackgroundResource(R.drawable.flechecdi);
             Intent intent = new Intent(HighScoolActivity.this, CdiActivity.class);
 
             startActivity(intent);
@@ -28,7 +34,7 @@ public class HighScoolActivity extends ActionBarActivity {
     private View.OnClickListener redirect_cvl = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            findViewById(R.id.fleche_cvl).setBackgroundResource(R.drawable.flechecvl);
+            findViewById(R.id.iv_highschool_fleche_cvl).setBackgroundResource(R.drawable.flechecvl);
             Intent intent = new Intent(HighScoolActivity.this, CvlActivity.class);
 
             startActivity(intent);
@@ -37,7 +43,7 @@ public class HighScoolActivity extends ActionBarActivity {
     private View.OnClickListener redirect_coordonnes = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            findViewById(R.id.fleche_contact).setBackgroundResource(R.drawable.flechecoo);
+            findViewById(R.id.iv_highschool_fleche_contact).setBackgroundResource(R.drawable.flechecoo);
             Intent intent = new Intent(HighScoolActivity.this, ContactActivity.class);
 
             startActivity(intent);
@@ -46,7 +52,7 @@ public class HighScoolActivity extends ActionBarActivity {
     private View.OnClickListener redirect_orientation = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            findViewById(R.id.fleche_orientation).setBackgroundResource(R.drawable.flecheorien);
+            findViewById(R.id.iv_highschool_fleche_orientation).setBackgroundResource(R.drawable.flecheorien);
             Intent intent = new Intent(HighScoolActivity.this, OrientationActivity.class);
 
             startActivity(intent);
@@ -55,7 +61,7 @@ public class HighScoolActivity extends ActionBarActivity {
     private View.OnClickListener redirect_infos = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            findViewById(R.id.fleche_infos).setBackgroundResource(R.drawable.flecheinfos);
+            findViewById(R.id.iv_highschool_fleche_infos).setBackgroundResource(R.drawable.flecheinfos);
             Intent intent = new Intent(HighScoolActivity.this, InfosActivity.class);
 
             startActivity(intent);
@@ -63,11 +69,11 @@ public class HighScoolActivity extends ActionBarActivity {
     };
 
     private void resetPointers(){
-        findViewById(R.id.fleche_cdi).setBackgroundResource(R.drawable.flechebase);
-        findViewById(R.id.fleche_cvl).setBackgroundResource(R.drawable.flechebase);
-        findViewById(R.id.fleche_contact).setBackgroundResource(R.drawable.flechebase);
-        findViewById(R.id.fleche_orientation).setBackgroundResource(R.drawable.flechebase);
-        findViewById(R.id.fleche_infos).setBackgroundResource(R.drawable.flechebase);
+        findViewById(R.id.iv_highschool_fleche_cdi).setBackgroundResource(R.drawable.flechebase);
+        findViewById(R.id.iv_highschool_fleche_cvl).setBackgroundResource(R.drawable.flechebase);
+        findViewById(R.id.iv_highschool_fleche_contact).setBackgroundResource(R.drawable.flechebase);
+        findViewById(R.id.iv_highschool_fleche_orientation).setBackgroundResource(R.drawable.flechebase);
+        findViewById(R.id.iv_highschool_fleche_infos).setBackgroundResource(R.drawable.flechebase);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,18 +82,26 @@ public class HighScoolActivity extends ActionBarActivity {
         setCustomActionBar();
         setContentView(R.layout.activity_high_scool);
 
-        cdi = (RelativeLayout) findViewById(R.id.relative_CDI);
-        cvl = (RelativeLayout) findViewById(R.id.relative_CVL);
-        coordonnes = (RelativeLayout) findViewById(R.id.relative_coordonnes);
-        orientation = (RelativeLayout) findViewById(R.id.relative_orientation);
-        infos = (RelativeLayout) findViewById(R.id.relative_infos);
+        cdi = (LinearLayout) findViewById(R.id.ll_highschool_cdi);
+        cvl = (LinearLayout) findViewById(R.id.ll_highschool_cvl);
+        coordonnes = (LinearLayout) findViewById(R.id.ll_highschool_contact);
+        orientation = (LinearLayout) findViewById(R.id.ll_highschool_orientation);
+        infos = (LinearLayout) findViewById(R.id.ll_highschool_infos);
 
         cdi.setOnClickListener(redirect_cdi);
         cvl.setOnClickListener(redirect_cvl);
         coordonnes.setOnClickListener(redirect_coordonnes);
         orientation.setOnClickListener(redirect_orientation);
         infos.setOnClickListener(redirect_infos);
+        
+        setLastUpdates();
 
+    }
+
+    private void setLastUpdates() {
+        ((TextView)findViewById(R.id.tv_highschool_cdi)).append(new DataCDIProvider(this).getLastDate());
+        ((TextView)findViewById(R.id.tv_highschool_cvl)).append(new DataCVLProvider(this).getLastDate());
+        ((TextView)findViewById(R.id.tv_highschool_infos)).append(new DataHighSchoolProvider(this).getLastDate());
     }
 
     @Override
